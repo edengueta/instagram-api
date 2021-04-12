7664457284061b2d9393b85dc659bdba94733813
“Your <you@example.com>
2021-04-13 01:44:42 +0300

    make it better

--- src/config/cloudinary/cloudinary.config.js
+++ src/config/cloudinary/cloudinary.config.js
@@ -1,11 +1,14 @@
-// const api_secret = require("./api_secret") || "";
 const cloudinary = require('cloudinary');
-
+try {
+  const api_secret = require("./api_secret");
+} catch(e) {
+  console.log(e)
+}
 
 cloudinary.config({ 
     cloud_name: 'edengueta', 
     api_key: '252315661325637', 
-    api_secret: process.env.CLOUDINARY_API_SECRET
+    api_secret: process.env.CLOUDINARY_API_SECRET || api_secret
   });
   
   module.exports = cloudinary;
